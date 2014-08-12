96f283e7936f910fa3808bdcb223dc7ed2de1c14
seasox <github@jboy.eu>
2014-08-12 18:27:37 +0200

    The screenshots per second are adjustable now.

--- make_video.sh
+++ make_video.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export TMP_DIRECTORY="sorted"
+SCREENSHOTS_PER_SECOND=1
 VIDEO_FNAME="output.mp4"
 VIDEO_FPS=24
 VIDEO_HEIGHT=720
@@ -17,4 +18,4 @@ ls ingr-*.png | gawk 'BEGIN{ a=1 }{ printf "ln -s ../%s ${TMP_DIRECTORY}/ingr_%0
 
 cd $TMP_DIRECTORY
 
-avconv -r 1 -f image2 -i ingr_%04d.png -c:v libx264 -maxrate:v 20000k -minrate:v 16000k  -c:a n -s ${VIDEO_WIDTH}x${VIDEO_HEIGHT} -r $VIDEO_FPS $VIDEO_FNAME
+avconv -r ${SCREENSHOTS_PER_SECOND} -f image2 -i ingr_%04d.png -c:v libx264 -maxrate:v 20000k -minrate:v 16000k  -c:a n -s ${VIDEO_WIDTH}x${VIDEO_HEIGHT} -r $VIDEO_FPS $VIDEO_FNAME
